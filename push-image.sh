d0c187f3286268a62a5ff4d84c47cb8e12e1b976
sagiransari <tech.sagir@gmail.com>
2025-04-19 15:57:59 +0530

    added ci pileline

--- push-image.sh
+++ push-image.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-./gradlew bootBuildImage --imageName ghcr.io/<your_github_username>/catalog-service \
---publishImage \
+./gradlew bootBuildImage --imageName ghcr.io/mdsagir/catalog-service --publishImage \
 -PregistryUrl=ghcr.io \
--PregistryUsername=<your_github_username> \
--PregistryToken=<your_github_token>
+-PregistryUsername=mdsagir \
+-PregistryToken=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
